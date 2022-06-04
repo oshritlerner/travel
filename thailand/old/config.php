90a31642dc75130ba26d6b0f85c66d9e4260e98d
ronenno1 <ronenno1@gmail.com>
2022-06-04 15:48:32 +0300

    add thailand!

--- thailand/old/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	Class config
-	{
-			const year = '2008';
-			const start_date = '2008-07-28';
-			const last_date = '2008-09-15';
-			public static $users = array('ronen'=>'0508383423', 'oshrit'=>'0506411478');
-	}
-
